9bd75815fc001dc2227bd1b3469f2e3473efa6bf
izcet <isaacjrhett@gmail.com>
2017-08-24 18:19:52 -0700

    [src/desecb_u.c] [inc/ft_des.h]

--- inc/ft_des.h
+++ inc/ft_des.h
@@ -6,7 +6,7 @@
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/21 16:16:13 by irhett            #+#    #+#             */
-/*   Updated: 2017/08/23 02:45:47 by irhett           ###   ########.fr       */
+/*   Updated: 2017/08/24 18:13:56 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 
 typedef struct		s_des
 {
-	int				mode:2;
-	char			*string;
+	int				decode:2;
+	int				base64:2;
+	char			*infile;
+	char			*outfile;
+	char			*key;
 }					t_des;
 
 typedef struct s_com	t_com;
--- src/desecb_u.c
+++ src/desecb_u.c
@@ -5,15 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/22 17:57:38 by irhett            #+#    #+#             */
-/*   Updated: 2017/08/22 17:58:50 by irhett           ###   ########.fr       */
+/*   Created: 2017/08/22 16:39:18 by irhett            #+#    #+#             */
+/*   Updated: 2017/08/24 18:18:45 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ftssl.h"
 
-void	desecb_u(t_com *command)
+void		desecb_u(t_com *command)
 {
-	(void)command;
-	ft_putendl("Usage: des [flesh out the usage here]");
+	ft_putstr_fd("Usage: ", 2);
+	ft_putstr_fd(command->name);
+	ft_putendl_fd(" [-e|-d] [-a] [-k pass] [-i|-in infile] [-o|-out outfile]");
+	ft_putendl_fd("  -e, encode (default behavior)", 2);
+	ft_putendl_fd("  -d, decode", 2);
+	ft_putendl_fd("  -a, base64 encode/decode, depending on the previous flag");
+	ft_putendl_fd("  -i, -in, input file (\"-\" or default: stdin)", 2);
+	ft_putendl_fd("  -o, -out, output file (\"-\" or default: stdout)", 2);
 }
