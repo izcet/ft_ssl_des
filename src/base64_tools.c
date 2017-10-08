4a13d45fce4438d7d74f6cde0ce67d3e3134487c
izcet <isaacjrhett@gmail.com>
2017-10-07 22:52:29 -0700

    [src/base64_tools.c]

--- src/base64_tools.c
+++ src/base64_tools.c
@@ -6,7 +6,7 @@
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/22 16:37:49 by irhett            #+#    #+#             */
-/*   Updated: 2017/09/15 16:59:47 by irhett           ###   ########.fr       */
+/*   Updated: 2017/10/07 22:51:52 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,43 @@ void			three_to_four(unsigned char *str, char *key,
 		unsigned char *enc, int num)
 {
 	int		i;
-	int		past_done;
+	int		done;
 
 	i = 0;
-	past_done = 0;
+	done = 0;
 	enc[0] = key[(str[i++] & 252) >> 2];
 	num--;
 	enc[1] = key[((str[i - 1] & 3) << 4) | (str[i] >> 4)];
 	if (!num)
-		past_done = 1;
+		done = 1;
 	if (num)
 	{
 		i++;
 		num--;
 	}
-	enc[2] = past_done ? '=' : key[((str[i] & 192) >> 6) | ((str[i - 1] & 15) << 2)];
+	enc[2] = done ? '=' : key[((str[i] & 192) >> 6) | ((str[i - 1] & 15) << 2)];
 	if (!num)
-		past_done = 1;
-	enc[3] = past_done ? '=' : key[str[i] & 63];
+		done = 1;
+	enc[3] = done ? '=' : key[str[i] & 63];
+}
+
+static int		copy_over(unsigned char *c, unsigned char *e, unsigned char *s)
+{
+	int		i;
+	int		ret;
+
+	ret = 0;
+	i = -1;
+	while (++i < 4)
+		if (e[i] == '=')
+		{
+			ret++;
+			c[i] = 0;
+		}
+	s[0] = (c[0] << 2) | (c[1] >> 4);
+	s[1] = ((c[1] & 15) << 4) | (c[2] >> 2);
+	s[2] = ((c[2] & 3) << 6) | c[3];
+	return (ret);
 }
 
 int				four_to_three(unsigned char *str, unsigned char *enc, char *key)
@@ -41,7 +60,6 @@ int				four_to_three(unsigned char *str, unsigned char *enc, char *key)
 	int				i;
 	int				c_i;
 	unsigned char	c[4];
-	int				ret;
 
 	i = -1;
 	while (++i < 4)
@@ -58,18 +76,7 @@ int				four_to_three(unsigned char *str, unsigned char *enc, char *key)
 			if (enc[c_i] == key[i])
 				c[c_i] = i;
 	}
-	ret = 0;
-	i = -1;
-	while (++i < 4)
-		if (enc[i] == '=')
-		{
-			ret++;
-			c[i] = 0;
-		}
-	str[0] = (c[0] << 2) | (c[1] >> 4);
-	str[1] = ((c[1] & 15) << 4) | (c[2] >> 2);
-	str[2] = ((c[2] & 3) << 6) | c[3];
-	return (ret);
+	return (copy_over(c, enc, str));
 }
 
 unsigned int	base64_trim(char *str)
