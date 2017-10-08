d7cc73d560330649c9538c3d823f03e6bc317b85
izcet <isaacjrhett@gmail.com>
2017-10-07 23:26:34 -0700

    [libft/src/ft_strnew.c]

--- libft/src/ft_strnew.c
+++ libft/src/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 13:32:54 by irhett            #+#    #+#             */
-/*   Updated: 2017/09/13 21:36:16 by irhett           ###   ########.fr       */
+/*   Updated: 2017/10/07 23:25:21 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ char	*ft_strnew(size_t size)
 {
 	char	*str;
 	size_t	i;
+
 	str = (char*)malloc(sizeof(char) * (size + 1));
 	i = 0;
 	if (str)
